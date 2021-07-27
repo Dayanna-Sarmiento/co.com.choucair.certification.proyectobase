b40880768f616f96a7f446536615b94eed73e1be
Dayanna-Sarmiento <lorendasar@gmail.com>
2021-07-27 13:22:26 -0500

    Cap. 1 - Primeros PasosLibro Cap. 1.1 - Crear Proyecto base en IntelliJLibro Cap. 2 - Base TeóricaLibro Cap. 3 - Feature StepDefinition y Configuración InicialLibro Cap. 4 - Implementando nuestra primera tareaLibro Cap. 5 - Identificar y MapearLibro Cap. 6 - Implementación de QuestionsLibro Cap. 6.1 - Implementación datadriven y clase ModelLibro

--- src/test/java/co/com/choucair/certification/proyectobase/stepdefinitions/ChoucairAcademyStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/proyectobase/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -1,5 +1,6 @@
 package co.com.choucair.certification.proyectobase.stepdefinitions;
 
+import co.com.choucair.certification.proyectobase.model.AcademyChoucairData;
 import co.com.choucair.certification.proyectobase.questions.Answer;
 import co.com.choucair.certification.proyectobase.tasks.Login;
 import co.com.choucair.certification.proyectobase.tasks.OpenUp;
@@ -12,13 +13,15 @@ import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
+import java.util.List;
+
 public class ChoucairAcademyStepDefinitions {
     @Before
     public void setStage(){
         OnStage.setTheStage(new OnlineCast());
     }
 
-    @Given("^than brandon wants to learn automation at the academy Choucair$")
+/*    @Given("^than brandon wants to learn automation at the academy Choucair$")
     public void thanBrandonWantsToLearnAutomationAtTheAcademyChoucair() {
         OnStage.theActorCalled("Brandon").wasAbleTo(OpenUp.thePage(), (Login.onThePage()));
     }
@@ -31,5 +34,19 @@ public class ChoucairAcademyStepDefinitions {
     @Then("^he finds the course called resources (.*)$")
     public void heFindsTheCourseCalledResourcesRecursosAutomatizacionBancolombia(String question) {
         OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(question)));
+    }*/
+    @Given("^than Rose want to learn automation at the academy Choucair$")
+    public void thanRoseWantToLearnAutomationAtTheAcademyChoucair(List<AcademyChoucairData> academyChoucairData) throws Exception {
+        OnStage.theActorCalled("Rose").wasAbleTo(OpenUp.thePage(), Login.onThePage(academyChoucairData.get(0).getStrUser(), academyChoucairData.get(0).getStrPassword()));
+    }
+
+    @When("^she search for the course on the choucair academy platform$")
+    public void sheSearchForTheCourseOnTheChoucairAcademyPlatform(List<AcademyChoucairData> academyChoucairData) throws Exception {
+        OnStage.theActorInTheSpotlight().attemptsTo(Search.the(academyChoucairData.get(0).getStrCourse()));
+    }
+
+    @Then("^she finds the course called$")
+    public void sheFindsTheCourseCalled(List<AcademyChoucairData> academyChoucairData) throws Exception {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(academyChoucairData.get(0).getStrCourse())));
     }
 }
--- src/test/resources/features/academyChoucair.feature
+++ src/test/resources/features/academyChoucair.feature
@@ -3,7 +3,18 @@
 Feature: Academy Choucair
   As a user, I want to learn how to automate in screamplay at the Choucair Academy with the automation course
   @scenario1
+#  Scenario: Search for a automation course
+#    Given than brandon wants to learn automation at the academy Choucair
+#    When he search for the course Recursos Automatización Bancolombia on the choucair academy platform
+#    Then he finds the course called resources Recursos Automatización Bancolombia
+
   Scenario: Search for a automation course
-    Given than brandon wants to learn automation at the academy Choucair
-    When he search for the course Recursos Automatización Bancolombia on the choucair academy platform
-    Then he finds the course called resources Recursos Automatización Bancolombia
+    Given than Rose want to learn automation at the academy Choucair
+      | strUser  | strPassword |
+      | username | password    |
+    When she search for the course on the choucair academy platform
+      | strCourse               |
+      | MetodoLogin Bancolombia |
+    Then she finds the course called
+      | strCourse               |
+      | MetodoLogin Bancolombia |
